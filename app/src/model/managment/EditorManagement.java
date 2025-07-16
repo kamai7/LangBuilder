cd98410e0dcc94d764f5d7e1c5ac23382bc3dbd7
kamai7 <anaellecarre35@gmail.com>
2025-07-16 23:58:02 +0200

    début de word editor

--- app/src/model/managment/EditorManagement.java
+++ app/src/model/managment/EditorManagement.java
@@ -12,12 +12,10 @@ public class EditorManagement {
         FXMLHandler<BorderPane, EditorController> editor = new FXMLHandler<>("/fxml/static/editor.fxml");
         EditorController controller = editor.getController();
         controller.setHeaderObject("φʹ");
-        controller.setHeaderName("Letter:");
+        controller.setHeaderName("Letter: ");
         controller.setHeaderObjectStyle("-fx-text-fill: radial-gradient(focus-distance 0%, center 60% 100%, radius 80%, rgb(255, 0, 234), rgb(255, 187, 0))");
 
         FXMLHandler<VBox, LetterEditorController> letterEditor = new FXMLHandler<>("/fxml/static/letter_editor.fxml");
-        LetterEditorController letterController = letterEditor.getController();
-        letterController.initValues("φʹ", "phi'");
         controller.setContent(letterEditor.get());
 
         return editor;
