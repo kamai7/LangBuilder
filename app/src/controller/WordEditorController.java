7c0cb9e88503cccbcfc906c29e4e167e6767680c
kamai7 <anaellecarre35@gmail.com>
2025-07-18 01:40:44 +0200

    correction de pb dans les sliders

--- app/src/controller/WordEditorController.java
+++ app/src/controller/WordEditorController.java
@@ -1,20 +1,15 @@
 package controller;
 
 
-import java.text.ParseException;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import model.util.Colors;
-import model.util.Utils;
 
 public class WordEditorController {
 
@@ -61,56 +56,42 @@ public class WordEditorController {
             lengthSlider.setValue(newValue.intValue());
         });
         emotionalitySlider.valueProperty().addListener((ChangeListener<Number>) (ovn, oldValue, newValue) -> {
-            float rounded = Math.round(newValue.doubleValue() * 100.0) / 100.0f;
-            emotionalityValue.setText(String.format("%.2f", rounded));
-            emotionalitySlider.setValue(rounded);
+            float rounded = Math.round(newValue.doubleValue() * 20.0) / 20.0f;
+            emotionalityValue.setText(rounded + "");
         });
         vulgaritySlider.valueProperty().addListener((ChangeListener<Number>) (ovn, oldValue, newValue) -> {
-            float rounded = Math.round(newValue.doubleValue() * 100.0) / 100.0f;
-            vulgarityValue.setText(String.format("%.2f", rounded));
-            vulgaritySlider.setValue(rounded);
+            float rounded = Math.round(newValue.doubleValue() * 20.0) / 20.0f;
+            vulgarityValue.setText(rounded + "");
         });
         formalitySlider.valueProperty().addListener((ChangeListener<Number>) (ovn, oldValue, newValue) -> {
-            float rounded = Math.round(newValue.doubleValue() * 100.0) / 100.0f;
-            formalityValue.setText(String.format("%.2f", rounded));
-            formalitySlider.setValue(rounded);
+            float rounded = Math.round(newValue.doubleValue() * 20.0) / 20.0f;
+            formalityValue.setText(rounded + "");
         });
 
         // Add listeners to text fields
-        lengthValue.setOnAction(event -> {
-            try {
+        lengthValue.textProperty().addListener(event -> {
+            try{
                 int value = Integer.parseInt(lengthValue.getText());
                 lengthSlider.setValue(value);
-            } catch (NumberFormatException e) {
-                lengthValue.setText(String.valueOf((int) lengthSlider.getValue())); // revert to old value if parsing fails
-            }
+            } catch (NumberFormatException e){}
         });
-        emotionalityValue.setOnAction(event -> {
+        emotionalityValue.textProperty().addListener(event -> {
             try {
-                float value = Utils.parseFloat(emotionalityValue.getText());
+                float value = Float.parseFloat(emotionalityValue.getText());
                 emotionalitySlider.setValue(value);
-            } catch (ParseException e) {
-                String value = Utils.formatNumber(emotionalitySlider.getValue());
-                emotionalityValue.setText(value);
-            }
+            } catch (NumberFormatException e) {}
         });
-        vulgarityValue.setOnAction(event -> {
+        vulgarityValue.textProperty().addListener(event -> {
             try {
-                float value = Utils.parseFloat(vulgarityValue.getText());
+                float value = Integer.parseInt(vulgarityValue.getText());
                 vulgaritySlider.setValue(value);
-            } catch (ParseException e) {
-                String value = Utils.formatNumber(vulgaritySlider.getValue());
-                vulgarityValue.setText(value);
-            }
+            } catch (NumberFormatException e) {}
         });
-        formalityValue.setOnAction(event -> {
+        formalityValue.textProperty().addListener(event -> {
             try {
-                float value = Utils.parseFloat(formalityValue.getText());
+                float value = Integer.parseInt(formalityValue.getText());
                 formalitySlider.setValue(value);
-            } catch (ParseException e) {
-                String value = Utils.formatNumber(formalitySlider.getValue());
-                System.out.println(value);
-                formalityValue.setText(value);
+            } catch (NumberFormatException e) {
             }
         });
     }
